e6c928e687d975766cea18a722d04f0ce74a0eef
Riccardo-Grasso <riccardo.grasso@outlook.it>
2022-01-17 18:04:05 +0100

    importato e creato i file per vue

---
+++ resources/js/components/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>{{ helloMsg }}</h1>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      helloMsg: "Benvenuto nel Blog di Boolean",
+    };
+  },
+};
+</script>
--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/* Route::get('/', function () {
     return view('welcome');
-});
+}); */
 
 Auth::routes();
 
@@ -27,5 +27,10 @@ Route::namespace("Admin")
     ->group(function () {
         Route::get('/', 'HomeController@index')->name('home');
 
-        //Route::resource("users", "UserController");
+        Route::resource("users", "UserController");
     });
+
+
+Route::get("{any?}", function () {
+    return view("guests.home");
+})->where("any", ".*");
