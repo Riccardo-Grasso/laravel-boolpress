79e641c1a7fa8ad52fe690a750fc57c06d7a52eb
Riccardo-Grasso <riccardo.grasso@outlook.it>
2022-01-18 17:00:59 +0100

    navbar

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,12 +1,19 @@
 <template>
-  <div class="title-container text-center my-5">
-    <h1 class="title">{{ helloMsg }}</h1>
+  <div>
+    <Navbar></Navbar>
+
+    <div class="title-container text-center my-5">
+      <h1 class="title">{{ helloMsg }}</h1>
+    </div>
   </div>
 </template>
 
 <script>
+import Navbar from "./partials/Navbar.vue";
+
 export default {
   name: "App",
+  components: { Navbar },
   data() {
     return {
       helloMsg: "Benvenuto nel Blog di Boolean",
@@ -14,3 +21,4 @@ export default {
   },
 };
 </script>
+
